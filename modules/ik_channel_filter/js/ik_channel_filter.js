29e269d48f97e8e547439d532596b5bc7ab43a0b
Jesper Kristensen <cableman@linuxdev.dk>
2012-11-06 10:30:52 +0100

    Fixed removed of data in channel filter fields

--- modules/ik_channel_filter/js/ik_channel_filter.js
+++ modules/ik_channel_filter/js/ik_channel_filter.js
@@ -1,19 +1,27 @@
 (function($) {
   $('document').ready(function() {
 
+    // Add handler to the remove item button.
     $("a.ik-channel-filter-remove").live("click", function(){
       var item = $(this).parent();
-      var value = item.parent().parent().find('input[type="hidden"]');
-
-      item.remove();
-
       var str = $('.value', item).text();
+      var value = item.parent().parent().find('input[type="hidden"]');
+      var current = value.val();
 
-      console.log(str);
-      value.val(value.val().replace(str, ''));
+      // Remove the value from the hidden field. This is some what a hack at the
+      // moment.
+      var regex = new RegExp(str + ",?");
+      current = current.replace(regex, '');
+      if (current.charAt(current.length-1) == ',') {
+        current = current.substr(0, current.length-1);
+      }
 
+      // Remove the item and update the hidden field.
+      item.remove();
+      value.val(current);
     });
 
+    // Handle add button.
     $('.ik-channel-filter-add-button').click(function(e) {
       e.preventDefault();
       var parent = $(this).parent();
@@ -22,6 +30,7 @@
       // @todo: validate with server that data is okay or hook into behavoiusr
       // and only enable on one hit from autocomplete.
 
+      // Update the hidden field and handle that it may be empty.
       var current = parent.find('input[type="hidden"]').val();
       if (current === '') {
         parent.find('input[type="hidden"]').val(data);
@@ -30,12 +39,13 @@
         parent.find('input[type="hidden"]').val(current + ',' + data);
       }
 
-      // @todo: validate with server that data is okay.
+      // Update the UI by adding the element.
       var element = $('<li><span class="value">' + data + '</span><a class="button ik-channel-filter-remove">'+Drupal.t('Remove')+'</a></li>');
       $(this).parent().find('.ik-channel-filter-results').append(element);
 
-      // Empty field.
+      // Empty the input field.
       parent.find('input[type="text"]').val('');
     });
+
   });
 })(jQuery);
