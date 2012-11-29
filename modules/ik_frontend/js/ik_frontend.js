7c2c1741fdb8abcc73d40bd78082306f9753c5db
Jesper Kristensen <cableman@linuxdev.dk>
2012-11-29 16:36:10 +0100

    Added an object outline for the frontend

--- modules/ik_frontend/js/ik_frontend.js
+++ modules/ik_frontend/js/ik_frontend.js
@@ -1,12 +1,58 @@
+// Define configuration object to get settings data from backend to frontend.
+// Before calls are made to the backend.
 var IKFrontend = IKFrontend || { 'settings': {} };
+
+// Object chanele {
+//
+//  Functions
+//  -- start
+//  -- stop
+//
+//  -- debug
+//  ---- enable/disable console log from console.
+//
+//
+//  -- fetch_channel()
+//  ---- also update
+//  ---- '/channels/' + IKFrontend.settings.token + '/update'
+//
+//  -- nextSlide()
+//  -- prevSlide()
+//  -- changeSlide(from sid, to sid, effect)
+//
+//
+//  Exposed outside object.
+//  -- start()
+//  -- stop().
+//  -- debug(on/off).
+//
+// }
+//
+// Object slide {
+//  Store render slide, cache, last updated ?
+//
+//  Functions
+//  -- fecth_slide(sid)
+//  ---- '/channels/' + IKFrontend.settings.token + '/slide/{sid}'
+//
+//  -- render().
+// }
+//
+//
+//}
+
+
 
 (function($){
   $(document).ready(function() {
+
+    // Create channel object
+
+    // Below is tmp debugging.
     console.log(IKFrontend.settings.token);
     $.ajax({
-      type: 'POST',
-      url: '/channels/'+IKFrontend.settings.token+'/update',
-      data: 'channelid',
+      type: 'GET',
+      url: '/channels/' + IKFrontend.settings.token + '/update',
       dataType: 'JSON',
       success: function(data) {
         console.log(data);
